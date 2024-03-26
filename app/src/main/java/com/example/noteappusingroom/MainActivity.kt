7aeb5d1ba831fc687652e335cf8bdfc97fb6b7e3
Rahul kumar maury <rahu7827759@gmail.com>
2024-03-26 14:07:17 +0530

    layout design. create Dao, Database and Entity. set Adapter. set up viewModel.

--- app/src/main/java/com/example/noteappusingroom/MainActivity.kt
+++ app/src/main/java/com/example/noteappusingroom/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.noteappusingroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.noteappusingroom.database.NoteDatabase
+import com.example.noteappusingroom.repository.NoteRepository
+import com.example.noteappusingroom.viewmodel.NoteViewModel
+import com.example.noteappusingroom.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setUpViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/example/noteappusingroom/viewmodel/NoteViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.noteappusingroom.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Query
+import com.example.noteappusingroom.model.Note
+import com.example.noteappusingroom.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(app: Application, private val noteRepository: NoteRepository): AndroidViewModel(app) {
+
+    fun addNote(note: Note) = viewModelScope.launch {
+        noteRepository.insertNote(note)
+    }
+
+
+    fun deleteNote(note: Note) = viewModelScope.launch {
+        noteRepository.deleteNote(note)
+    }
+
+
+    fun updateNote(note: Note) = viewModelScope.launch {
+        noteRepository.updateNote(note)
+    }
+
+    fun getAllNotes() = noteRepository.getAllNotes()
+
+    fun searchNote(query: String?) = noteRepository.searchNote(query)
+}
